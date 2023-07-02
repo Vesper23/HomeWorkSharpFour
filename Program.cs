eed3e0b29271084209d08cf4dadbe816fdd9d4a4
Valentin Likholat <phantom.231220@gmail.com>
2023-07-02 22:37:28 +0300

    Completed 1 Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*
+int Stepen(int number, int stepen)
+{
+    int res = number;
+    for (int i = 1; i < stepen; i++)
+    {
+        res *= number;
+    }
+    return res;
+}
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int num = InputNum("Введите целове число: ");
+int step = InputNum("Введите степень числа: ");
+int result = Stepen(num, step);
+Console.WriteLine($"Степень числа {num} равна {result}.");
+*/
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
