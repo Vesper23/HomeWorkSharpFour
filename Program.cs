dfe24e4e8c368de564b45c0af3650d3c273bcf41
Valentin Likholat <phantom.231220@gmail.com>
2023-07-02 23:15:08 +0300

    Completed 2 Task

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ int InputNum(string message)
     return int.Parse(Console.ReadLine()!);
 }
 
-int num = InputNum("Введите целове число: ");
+int num = InputNum("Введите целое число: ");
 int step = InputNum("Введите степень числа: ");
 int result = Stepen(num, step);
 Console.WriteLine($"Степень числа {num} равна {result}.");
@@ -25,3 +25,23 @@ Console.WriteLine($"Степень числа {num} равна {result}.");
 
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Sum(int num)
+{
+    int res = 0;
+    while (num != 0)
+    {
+        res += num % 10;
+        num /= 10;
+    }
+    return res;
+}
+
+int num = InputNum("Введите целое число: ");
+int result = Sum(num);
+Console.WriteLine($"Сумма цифр числа {num} равна {result}.");
