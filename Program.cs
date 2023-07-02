db990136ed1f52c9842c407d3fa168ea25f8d240
Valentin Likholat <phantom.231220@gmail.com>
2023-07-02 23:42:23 +0300

    Completed 3 Task

--- Program.cs
+++ Program.cs
@@ -23,8 +23,10 @@ int result = Stepen(num, step);
 Console.WriteLine($"Степень числа {num} равна {result}.");
 */
 
+
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+/*
 int InputNum(string message)
 {
     Console.Write(message);
@@ -45,3 +47,36 @@ int Sum(int num)
 int num = InputNum("Введите целое число: ");
 int result = Sum(num);
 Console.WriteLine($"Сумма цифр числа {num} равна {result}.");
+*/
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] ArrayRand(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int len = InputNum("Введите длину массива: ");
+int[] arr = ArrayRand(len);
+PrintArray(arr);
